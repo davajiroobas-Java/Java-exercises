a157f5efcadf449c402011d2ec58d959c52141be
Dawan <davaj.iroobas@gamil.com>
2026-04-25 12:24:44 +0200

    add Main class only

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+
+//Keep with   Tutorials Java  Booleans
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main (String [] args){
+
+
+
+
+
+
+
+    double radie = Math.ceil (Math.random() * 10);
+
+    double area = Math.PI * radie *radie;
+    double circumference= Math.ceil (3 * Math.PI * radie);
+
+
+    System.out.println("Radie " + radie);
+    System.out.println("Area " + area);
+    System.out.println("Circumference " + circumference);
+
+  }
+
+}
+
