6ff4504fc20fb4b560dfd0c24b005a22815d2115
Dawan <davaj.iroobas@gamil.com>
2026-04-25 19:35:48 +0200

    Scanner-age

---
+++ src/Scanner.java
@@ -0,0 +1,21 @@
+public class Scanner {
+  public static void main(String [] args){
+
+    System.out.println(" Write your age: ");
+    java.util.Scanner scan= new java.util.Scanner(System.in);
+
+    int age=scan.nextInt();
+
+    int month= age * 12;
+    int day= age * 365;
+    int minute = age * 365 * 24 * 60;
+
+    System.out.println("Age " + age);
+    System.out.println("Month " + month);
+    System.out.println("Day " + day);
+    System.out.println("Minute " + minute);
+
+    scan.close();
+
+  }
+}
